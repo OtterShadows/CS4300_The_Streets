eb7621092c98e87e051e68461ce71a4858524f1c
Ollie <derekethompson@gmail.com>
2026-03-11 22:35:20 -0400

    filter out redundant json files

--- data/json_to_csv.py
+++ data/json_to_csv.py
@@ -1,28 +1,59 @@
 import json
 import os
+import csv
 from pathlib import Path
 
 
-with open("data_set.csv", "w") as f:
+def json_to_csv1():
+    with open("data_set.csv", "w") as f:
+        directory_path = Path('./data/comments_data')
+        for file_path in directory_path.iterdir():
+            if file_path.is_file():
+                print(file_path.name)
+                # You can also read the file content directly
+                content = file_path.read_text()
+                json_content = json.loads(content)
+                for i in range(100):
+                    try:
+                        json_content['data'][i]
+                    except KeyError:
+                        continue
+                    comment_id = json_content['data'][i]['id']
+                    comment_text = json_content['data'][i]['body']
+                    cleaned_comment_text = comment_text.replace('\n', ' ').replace('\r', ' ').replace('"', '')
+                    comment_timestamp = json_content['data'][i]['created_utc']
+
+                    f.write(f'{comment_id},{comment_timestamp},\"{cleaned_comment_text}\"\n')
+    
+
+with open("piratefolk_comments.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(['id', 'timestamp', 'text'])
+
     directory_path = Path('./data/comments_data')
+
     for file_path in directory_path.iterdir():
         if file_path.is_file():
-            print(file_path.name)
-            # You can also read the file content directly
+            print(f"{file_path.name}")
             content = file_path.read_text()
             json_content = json.loads(content)
-            for i in range(100):
-                try:
-                    json_content['data'][i]
-                except KeyError:
-                    continue
-                comment_id = json_content['data'][i]['id']
-                comment_text = json_content['data'][i]['body']
-                cleaned_comment_text = comment_text.replace('\n', ' ').replace('\r', ' ').replace('"', '')
-                comment_timestamp = json_content['data'][i]['created_utc']
+            if 'data' not in json_content:
+                print(file_path.name + " does not contain 'data' key. Skipped.")
+                continue
 
-                f.write(f'{comment_id},{comment_timestamp},\"{cleaned_comment_text}\"\n')
+            for comment in json_content.get('data', []):
+                comment_id = comment.get('id')
+                comment_timestamp = comment.get('created_utc')
+                comment_text = comment.get('body', '')
+
+                cleaned_text = comment_text.replace('\n', ' ').replace('\r', ' ').replace('"', '')
+
+                writer.writerow([comment_id, comment_timestamp, cleaned_text])
+
+
     
+
+
     
 
     
