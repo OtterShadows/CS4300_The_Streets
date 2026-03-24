147bddbf7fa65f10642c087436d8a7eb7abfde2f
Gabby <138426880+Gabby23x@users.noreply.github.com>
2026-03-23 23:31:15 -0400

    Revert "Revert "searching for name now requires "name:" in query""

--- src/routes.py
+++ src/routes.py
@@ -20,50 +20,32 @@ tfidf_matrix = data["matrix"]
 vectorizer = data["vectorizer"]
 characters = data["characters"]
 
+def query_character(query):
+    query_vec = vectorizer.transform([query])
+    sims = cosine_similarity(query_vec, tfidf_matrix).flatten()
+    return characters[sims.argmax()]
+
+
+
 def json_search(query):
-    if not query or not query.strip():
-        query = "Luffy"
-    print("\033[32m" + "Query: " + query + "\033[0m")
-    name = similarity_calc.match_name(query, similarity_calc.char_list)
-    print("\033[32m" + "Name: " + name + "\033[0m")
-
-    matches = similarity_calc.retrieve_k_docs(name, similarity_calc.tfidf_matrix, 10, similarity_calc.vectorizer, similarity_calc.ids, similarity_calc.docs)
-    summary = "Summary to be implemented."
-    retrieved = matches
-
-    # character_score = similarity_calc.get_character_rating(name)
-
-    print("\033[32m" + "Calculating trend_data..." + "\033[0m")
-    trend_data = similarity_calc.get_star_rating_over_time(name, 5)
-    print("\033[32m" + "Calculating trend_stars..." + "\033[0m")
-    trend_stars = [round(v, 2) for v in trend_data.values()]
-    print("\033[32m" + "Calculating trend_dates..." + "\033[0m")
-    trend_dates = list(trend_data.keys())
+    """if not query or not query.strip():
+        query = "Kardashian"
+    results = db.session.query(Episode, Review).join(
+        Review, Episode.id == Review.id
+    ).filter(
+        Episode.title.ilike(f'%{query}%')
+    ).all()
+    matches = []
+    for episode, review in results:
+        matches.append({
+            'title': episode.title,
+            'descr': episode.descr,
+            'imdb_rating': review.imdb_rating
+        })
+    return json.dumps(matches)"""
 
 
-    print("\033[32m" + "Calculating rating..." + "\033[0m")
-    rating = sum(trend_stars) / len(trend_stars)
-    print("\033[32m" + "Rating: " + str(rating) + "\033[0m")
 
-    mentions = similarity_calc.num_mentions(name)
-    # mentions = 0
-    if rating >= 6:
-        consensus = "Positive"
-    elif rating <= 4:
-        consensus = "Negative"
-    else:
-        consensus = "Neutral"
-
-    return json.dumps({
-        "name": name,
-        "summary": summary,
-        "retrieved": matches,
-        "rating": rating,
-        "mentions": mentions,
-        "consensus": consensus,
-        "trend": trend_stars,
-        "trend_dates": trend_dates
-    })
 
 def register_routes(app):
     @app.route("/")
